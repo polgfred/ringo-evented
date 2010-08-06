1549dc3e29cc8a54138df739e55191798e69d7b5
Frederick Polgardy <fred@polgardy.com>
2010-08-06 08:57:08 -0500

    simplified stupid example, added something more interesting

---
+++ examples/drip.js
@@ -0,0 +1,30 @@
+include('ringo/scheduler');
+include('evented');
+include('evented/http');
+
+var server = new HttpServer({ port: 4321 });
+
+server.listen({
+  connect: function (conn) {
+    print('connected from', conn.remoteAddr);
+    conn.start(200, {
+      'content-type': 'text/plain'
+    });
+  },
+  data: function (conn, data) {
+    var times = 0;
+    (function drip() {
+      times++;
+      setTimeout(function () {
+        var promise = conn.write('drop\n');
+        if (times == 10) promise.thenClose();
+        else promise.then(drip);
+      }, 1000);
+    }());
+  },
+  disconnect: function () {
+    print('disconnected');
+  }
+});
+
+server.start();
--- examples/stupid.js
+++ examples/stupid.js
@@ -1,26 +1,11 @@
+include('ringo/scheduler');
 include('evented');
 include('evented/http');
 
 var server = new HttpServer({ port: 4321 });
 
-server.listen({
-  connect: function (conn) {
-    print('connection from', conn.remoteAddr);
-    conn.start(200, {
-      'content-type': 'text/plain'
-    });
-  },
-  data: function (conn, data) {
-    print(data);
-    conn.write('i got this\n');
-    conn.write('more1\n');
-    conn.write('more2\n');
-    conn.write('more3\n');
-    conn.write('more4\n').thenClose();
-  },
-  close: function () {
-    print('closed');
-  }
+server.listen('data', function (conn) {
+  conn.write('A ringo says what?\n').thenClose();
 });
 
 server.start();
