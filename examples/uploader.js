221eedc4ab5b2d9d399ebd0ceb62dd9a35f36bb0
Frederick Polgardy <fred@polgardy.com>
2010-08-13 09:37:30 -0500

    Added comments to the uploader to explain chunked vs.  non-chunked request handling

--- examples/uploader.js
+++ examples/uploader.js
@@ -1,3 +1,28 @@
+/*
+* A slightly contrived example of how to handle chunked and non-chunked requests in the same server.
+* 
+* The pseudocode, in a nutshell:
+*
+* - In the 'request' message handler, check to see whether the 'chunked' attribute is true.
+*   - If it is:
+*     - The request is chunked. (No kidding.)
+*     - The request has NO content, but everything else will be present (headers, params, etc.).
+*     - Begin processing the request.
+*     - In the 'chunk' message handler, check to see whether the 'last' attribute is true.
+*       - If it is:
+*         - This is the final chunk.
+*         - It has NO content, but may have trailing headers. (I know, a bit of an oxymoron. Also, these
+*           aren't implemented yet.)
+*         - Finish processing the request and close the connection.
+*       - If is it not:
+*         - The chunk contains content in its 'content' attribute.
+*         - Process the chunk and leave the connection open.
+*   - If it is not:
+*     - The request is not chunked.
+*     - The request contains all the content in its 'content' attribute.
+*     - Process the request and close the connection.
+*/
+
 include('evented/http');
 
 var server = new HttpServer({ port: 4321 });
