ba351cd25a7ebba87c5f82e40e1f1765336e0876
Frederick Polgardy <fred@polgardy.com>
2010-09-01 10:45:02 -0500

    updated drip server to new connection api

--- examples/drip.js
+++ examples/drip.js
@@ -4,25 +4,23 @@ include('evented/http');
 var server = new HttpServer({ port: 4321 });
 
 server.listen({
-  connect: function (conn) {
-    print('connected from', conn.remoteAddress.hostname);
-    conn.start(200, {
-      'content-type': 'text/plain'
-    });
-  },
   request: function (conn) {
+    conn.write({
+      status: 200,
+      headers: { 'content-type': 'text/plain' },
+      chunked: true
+    });
+
     var times = 0;
+
     (function drip() {
       times++;
       setTimeout(function () {
-        var promise = conn.write('drop\n');
+        var promise = conn.write({ content: 'drop\n' });
         if (times == 10) promise.thenClose();
         else promise.then(drip);
       }, 1000);
     }());
-  },
-  disconnect: function () {
-    print('disconnected');
   }
 });
 
